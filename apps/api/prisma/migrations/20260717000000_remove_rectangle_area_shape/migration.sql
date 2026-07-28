854fd617f3ca7d68078e48f61313b073b997eb15
Luan Raisson Miano de Aquino <42654434+Luanrma@users.noreply.github.com>
2026-07-28 17:50:55 -0300

    chore(prisma): remove superseded migrations

--- apps/api/prisma/migrations/20260717000000_remove_rectangle_area_shape/migration.sql
+++
@@ -1,7 +0,0 @@
-UPDATE "CampaignAreaTemplate"
-SET "shape" = 'LINE'
-WHERE "shape" = 'RECTANGLE';
-
-UPDATE "SceneAreaEffect"
-SET "configurationSnapshot" = jsonb_set("configurationSnapshot", '{shape}', '"LINE"'::jsonb)
-WHERE "configurationSnapshot"->>'shape' = 'RECTANGLE';
